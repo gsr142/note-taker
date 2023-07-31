c04916b0df3dba98e087f6a1c912dffc8e8405f0
gsr142 <gsr142@gmail.com>
2023-07-31 09:59:23 -0700

    post request fixed. Thanks Kyle

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const fs = require('fs');
 // Set express App
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 //const dbFilePath = path.join(__dirname, 'db', 'db.json');
 const existingNotes = require('./db/db.json')
 app.use(express.static('public'));
 app.use(express.json());
-
+app.use(express.urlencoded({extended:true}));
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
@@ -27,7 +27,7 @@ app.get('/api/notes', (req, res) => {
     
 });
 
-app.post('api/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
     const { title, text } = req.body;
     if (title && text){
         const newNote = {
@@ -51,6 +51,7 @@ app.post('api/notes', (req, res) => {
                             ? console.error(writeErr)
                             : console.info('Notes have been updated')
                 );
+                res.status(200).json("Success")
             }
         });
     };
